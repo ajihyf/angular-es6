df222c667fe7df0c4a51c7903b95fec25d77aacc
youfeng.hao <ajihyf@gmail.com>
2016-06-17 00:01:48 +0800

    [Parsing] literal-expressions

--- src/parse.js
+++ src/parse.js
@@ -1,8 +1,49 @@
 /* @flow */
 import _ from 'lodash';
 
-function isNumber(ch: string): boolean {
+function isNumber(ch: ?string): boolean {
+  if (ch == null) return false;
   return ch >= '0' && ch <= '9';
+}
+
+function isExpOperator(ch: ?string): boolean {
+  return ch === '-' || ch === '+' || isNumber(ch);
+}
+
+function isIdentifier(ch: ?string): boolean {
+  if (typeof ch !== 'string') return false;
+  return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch === '_' || ch === '$';
+}
+
+function isWhiteSpace(ch: ?string) {
+  return ch === ' ' || ch === '\r' || ch === '\t' ||
+    ch === '\n' || ch === '\v' || ch === '\u00A0';
+}
+
+const ESCAPE_MAP: { [key: string]: string } = {
+  'n': '\n',
+  'f': '\f',
+  'r': '\r',
+  't': '\t',
+  'v': '\v',
+  '\'': '\'',
+  '"': '"'
+};
+
+const stringEscapeRegex: RegExp = /[^ a-zA-Z0-9]/g;
+
+function stringEscapeFn(ch: string) {
+  const unicode = `0000${ch.charCodeAt(0).toString(16)}`;
+  return `\\u${unicode.slice(-4)}`;
+}
+
+function escape(value: any): any {
+  if (typeof value === 'string') {
+    return `'${value.replace(stringEscapeRegex, stringEscapeFn)}'`;
+  } else if (value === null) {
+    return 'null';
+  }
+  return value;
 }
 
 type LexToken = {
@@ -14,17 +55,27 @@ type LexToken = {
 class Lexer {
   text: string;
   index: number;
-  ch: string | null;
+  ch: ?string;
   tokens: LexToken[];
 
   readNumber() {
     let number: string = '';
     while (this.index < this.text.length) {
-      const ch = this.text.charAt(this.index);
-      if (isNumber(ch)) {
+      const ch = this.text.charAt(this.index).toLocaleLowerCase();
+      if (ch === '.' || isNumber(ch)) {
         number += ch;
       } else {
-        break;
+        const nextCh = this.peek();
+        const prevCh = _.last(number);
+        if (ch === 'e' && isExpOperator(nextCh)) {
+          number += ch;
+        } else if (prevCh === 'e' && isExpOperator(ch) && nextCh && isNumber(nextCh)) {
+          number += ch;
+        } else if (isExpOperator(ch) && prevCh === 'e' && (!nextCh || !isNumber(nextCh))) {
+          throw new Error('Invalid exponent');
+        } else {
+          break;
+        }
       }
       this.index++;
     }
@@ -34,7 +85,64 @@ class Lexer {
     });
   }
 
-  lex(text): LexToken[] {
+  readString(quote: string) {
+    this.index++;
+    let str: string = '';
+    let escape: boolean = false;
+    while (this.index < this.text.length) {
+      const ch = this.text.charAt(this.index);
+      if (escape) {
+        if (ch === 'u') {
+          const hex = this.peek(4);
+          this.index += 4;
+          if (hex == null || !hex.match(/[\da-f]{4}/i)) {
+            throw new Error('Invalid unicode escape');
+          }
+          str += String.fromCharCode(parseInt(hex, 16));
+        } else {
+          const replacement = ESCAPE_MAP[ch];
+          if (replacement) {
+            str += replacement;
+          } else {
+            str += ch;
+          }
+        }
+        escape = false;
+      } else if (ch === quote) {
+        this.index++;
+        this.tokens.push({
+          text: str,
+          value: str
+        });
+        return;
+      } else if (ch === '\\') {
+        escape = true;
+      } else {
+        str += ch;
+      }
+      this.index++;
+    }
+    throw new Error('Unmatched Quote');
+  }
+
+  readIdentifier() {
+    let text = '';
+    while (this.index < this.text.length) {
+      const ch = this.text.charAt(this.index);
+      if (isIdentifier(ch) || isNumber(ch)) {
+        text += ch;
+      } else {
+        break;
+      }
+      this.index++;
+    }
+    this.tokens.push({
+      text,
+      identifier: true
+    });
+  }
+
+  lex(text: string): LexToken[] {
     this.text = text;
     this.index = 0;
     this.ch = null;
@@ -42,8 +150,20 @@ class Lexer {
 
     while (this.index < this.text.length) {
       this.ch = this.text.charAt(this.index);
-      if (isNumber(this.ch)) {
+      if (isNumber(this.ch) ||
+        (this.chIsIn('.') && isNumber(this.peek()))) {
         this.readNumber();
+      } else if (this.ch != null && this.chIsIn('\'"')) {
+        this.readString(this.ch);
+      } else if (this.ch != null && this.chIsIn('[],{}:')) {
+        this.tokens.push({
+          text: this.ch
+        });
+        this.index++;
+      } else if (isIdentifier(this.ch)) {
+        this.readIdentifier();
+      } else if (isWhiteSpace(this.ch)) {
+        this.index++;
       } else {
         throw new Error(`Unexpected next character: ${this.ch}`);
       }
@@ -51,9 +171,26 @@ class Lexer {
 
     return this.tokens;
   }
+
+  peek(offset: number = 1): ?string {
+    if (this.index > this.text.length - offset) {
+      return null;
+    }
+    return this.text.substr(this.index + 1, offset);
+  }
+
+  chIsIn(str: string) {
+    if (!this.ch) return false;
+    return str.indexOf(this.ch) >= 0;
+  }
 }
 
-type ASTNode = ASTProgramNode | ASTLiteralNode;
+type ASTNode = ASTProgramNode
+  | ASTLiteralNode
+  | ASTArrayExpressionNode
+  | ASTObjectNode
+  | ASTPropertyNode
+  | ASTIdentifierNode;
 
 type ASTProgramNode = {
   type: 'Program',
@@ -65,17 +202,43 @@ type ASTLiteralNode = {
   value: any
 };
 
+type ASTArrayExpressionNode = {
+  type: 'ArrayExpression',
+  elements: ASTNode[]
+};
+
+type ASTObjectNode = {
+  type: 'ObjectExpression',
+  properties: ASTPropertyNode[]
+};
+
+type ASTPropertyNode = {
+  type: 'Property',
+  key: ASTLiteralNode | ASTIdentifierNode,
+  value: ASTNode
+};
+
+type ASTIdentifierNode = {
+  type: 'Identifier',
+  name: string
+};
+
 class AST {
+  static Program = 'Program';
+  static Literal = 'Literal';
+  static ArrayExpression = 'ArrayExpression';
+  static ObjectExpression = 'ObjectExpression';
+  static Property = 'Property';
+  static Identifier = 'Identifier';
+
+  static constants: { [key: string]: ASTLiteralNode } = {
+    'null': { type: AST.Literal, value: null },
+    'true': { type: AST.Literal, value: true },
+    'false': { type: AST.Literal, value: false }
+  };
+
   lexer: Lexer;
   tokens: LexToken[];
-
-  program(): ASTProgramNode {
-    return { type: 'Program', body: this.constant() };
-  }
-
-  constant(): ASTLiteralNode {
-    return { type: 'Literal', value: this.tokens[0].value || '' };
-  }
 
   constructor(lexer: Lexer) {
     this.lexer = lexer;
@@ -84,6 +247,91 @@ class AST {
   ast(text): ASTNode {
     this.tokens = this.lexer.lex(text);
     return this.program();
+  }
+
+  program(): ASTProgramNode {
+    return { type: AST.Program, body: this.primary() };
+  }
+
+  primary(): ASTNode {
+    if (this.expect('[')) {
+      return this.arrayDeclaration();
+    }
+    if (this.expect('{')) {
+      return this.object();
+    }
+    if (AST.constants.hasOwnProperty(this.tokens[0].text)) {
+      return AST.constants[this.consume().text];
+    }
+    return this.constant();
+  }
+
+  peek(e: ?string): ?LexToken {
+    if (this.tokens.length > 0) {
+      if (this.tokens[0].text === e || !e) {
+        return this.tokens[0];
+      }
+    }
+  }
+
+  expect(e: ?string): ?LexToken {
+    if (this.tokens.length > 0) {
+      if (this.tokens[0].text === e || !e) {
+        return this.tokens.shift();
+      }
+    }
+  }
+
+  consume(e: ?string): LexToken {
+    const token = this.expect(e);
+    if (!token) {
+      throw new Error(`Unexpected. Expected ${e}`);
+    }
+    return token;
+  }
+
+  object(): ASTObjectNode {
+    const properties: ASTPropertyNode[] = [];
+    if (!this.peek('}')) {
+      do {
+        const peek = this.peek();
+        if (peek == null) {
+          throw new Error('Unexpected terminates.');
+        }
+        const key = peek.identifier ? this.identifier() : this.constant();
+        this.consume(':');
+        const value = this.primary();
+        properties.push({
+          type: AST.Property,
+          key,
+          value
+        });
+      } while (this.expect(','));
+    }
+    this.consume('}');
+    return { type: AST.ObjectExpression, properties };
+  }
+
+  arrayDeclaration(): ASTArrayExpressionNode {
+    const elements: ASTNode[] = [];
+    if (!this.peek(']')) {
+      do {
+        if (this.peek(']')) {
+          break;
+        }
+        elements.push(this.primary());
+      } while (this.expect(','));
+    }
+    this.consume(']');
+    return { type: AST.ArrayExpression, elements };
+  }
+
+  constant(): ASTLiteralNode {
+    return { type: AST.Literal, value: this.consume().value };
+  }
+
+  identifier(): ASTIdentifierNode {
+    return { type: AST.Identifier, name: this.consume().text };
   }
 }
 
@@ -108,11 +356,22 @@ class ASTCompiler {
 
   recurse(ast: ASTNode): any {
     switch (ast.type) {
-      case 'Literal':
-        return ast.value;
-      case 'Program':
+      case AST.Literal:
+        return escape(ast.value);
+      case AST.Program:
         this.state.body.push(`return ${this.recurse(ast.body)};`);
         break;
+      case AST.ArrayExpression:
+        const elements = _.map(ast.elements, element => this.recurse(element));
+        return `[${elements.join(',')}]`;
+      case AST.ObjectExpression:
+        const properties = _.map(ast.properties, property => {
+          const key = property.key.type === AST.Identifier
+            ? property.key.name : escape(property.key.value);
+          const value = this.recurse(property.value);
+          return `${key}:${value}`;
+        });
+        return `{${properties.join(',')}}`;
       default:
         throw new Error('Unknown ASTNode type');
     }
--- test/parse.spec.js
+++ test/parse.spec.js
@@ -1,13 +1,142 @@
 /* eslint-env mocha */
 import parse from '../src/parse';
 import { expect } from 'chai';
-import sinon from 'sinon';
-import _ from 'lodash';
 
-describe('parse', function () {
-  it('can parse an integer', function () {
-    const fn = parse('233');
-    expect(fn).to.be.a('function');
+describe('Parse', function () {
+  describe('number', function () {
+    it('can parse an integer', function () {
+      const fn = parse('233');
+      expect(fn).to.be.a('function');
+      expect(fn()).to.equal(233);
+    });
+
+    it('can parse a floating point number', function () {
+      const fn = parse('23.3');
+      expect(fn()).to.equal(23.3);
+    });
+
+    it('can parse a floating number without an integer part', function () {
+      const fn = parse('.233');
+      expect(fn()).to.equal(0.233);
+    });
+
+    it('can parse a number with scientific notation', function () {
+      const fn = parse('233e2');
+      expect(fn()).to.equal(23300);
+    });
+
+    it('can parse scientific notation with a float coefficient', function () {
+      const fn = parse('.233e2');
+      expect(fn()).to.equal(23.3);
+    });
+
+    it('can parse scientific notation with negative', function () {
+      const fn = parse('.233e2');
+      expect(fn()).to.equal(23.3);
+    });
+
+    it('can parse scientific notation + sign', function () {
+      const fn = parse('.233e+2');
+      expect(fn()).to.equal(23.3);
+    });
+
+    it('can parse upper case scientific notation', function () {
+      const fn = parse('.233E2');
+      expect(fn()).to.equal(23.3);
+    });
+
+    it('will not parse invalid scientific notation', function () {
+      expect(() => { parse('233e-'); }).to.throw();
+      expect(() => { parse('233e-a'); }).to.throw();
+    });
+  });
+
+  describe('string', function () {
+    it('can parse a string in single quotes', function () {
+      const fn = parse('\'abc\'');
+      expect(fn()).to.equal('abc');
+    });
+
+    it('can parse a string in double quotes', function () {
+      const fn = parse('"abc"');
+      expect(fn()).to.equal('abc');
+    });
+
+    it('will not parse a string with mismatching quotes', function () {
+      expect(() => { parse('"abs\''); }).to.throw();
+    });
+
+    it('can parse a string with single quotes inside', function () {
+      const fn = parse('\'a\\\'b\'');
+      expect(fn()).to.equal('a\'b');
+    });
+
+    it('can parse a string with double quotes inside', function () {
+      const fn = parse('"a\\\"b"');  // eslint-disable-line no-useless-escape
+      expect(fn()).to.equal('a\"b'); // eslint-disable-line no-useless-escape
+    });
+
+    it('will parse a string with unicode escape', function () {
+      const fn = parse('"\\u00A0"');
+      expect(fn()).to.equal('\u00A0');
+    });
+
+    it('will not parse a string with invalid unicode escapes', function () {
+      expect(() => { parse('"\\u00T0"'); }).to.throw();
+    });
+  });
+
+  it('will parse null', function () {
+    const fn = parse('null');
+    expect(fn()).to.be.null;
+  });
+
+  it('will parse true', function () {
+    const fn = parse('true');
+    expect(fn()).to.be.true;
+  });
+
+  it('will parse false', function () {
+    const fn = parse('false');
+    expect(fn()).to.be.false;
+  });
+
+  it('ignores whitespaces', function () {
+    const fn = parse(' \r\t\v\u00A0\n233');
     expect(fn()).to.equal(233);
+  });
+
+  describe('array', function () {
+    it('will parse an empty array', function () {
+      const fn = parse('[]');
+      expect(fn()).to.be.an('array').and.is.empty;
+    });
+
+    it('will parse a non-empty array', function () {
+      const fn = parse('[1, "two", [3], true]');
+      expect(fn()).to.deep.equal([1, 'two', [3], true]);
+    });
+
+    it('will parse an array with trailing comma', function () {
+      const fn = parse('[1, 2, 3, ]');
+      expect(fn()).to.deep.equal([1, 2, 3]);
+    });
+  });
+
+  describe('object', function () {
+    it('will parse an empty object', function () {
+      const fn = parse('{}');
+      expect(fn()).to.be.an('object').and.is.empty;
+    });
+
+    it('will parse a non-empty object', function () {
+      const fn = parse('{ "some key": 1, \'another-key\': true }');
+      expect(fn()).to.deep.equal({ 'some key': 1, 'another-key': true });
+    });
+
+    it('will parse an object with identifier keys', function () {
+      const fn = parse('{ a: 1, b: [0, 1], c: { d: 4 } }');
+      expect(fn()).to.deep.equal({ a: 1, b: [0, 1], c: { d: 4 } });
+    });
   });
 });
